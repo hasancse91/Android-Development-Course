c79a3463a35464a167566c3b02ff3db07677674a
Hasan Abdullah <hasan_cse91@yahoo.com>
2017-09-15 15:07:30 +0600

    sixth class

---
+++ 06-SixthClass/app/src/main/java/com/hellohasan/a06_sixthclass/ListView/AndroidTopicListActivity.java
@@ -0,0 +1,47 @@
+package com.hellohasan.a06_sixthclass.ListView;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.hellohasan.a06_sixthclass.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class AndroidTopicListActivity extends AppCompatActivity {
+
+    @BindView(R.id.topicListView)
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_topic_list);
+        ButterKnife.bind(this);
+
+        final String[] topicList = getResources().getStringArray(R.array.topic_list);
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, topicList);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), topicList[i], Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), "Long: " + topicList[i], Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+}
